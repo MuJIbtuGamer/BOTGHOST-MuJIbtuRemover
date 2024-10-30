3675912d61e5c4e4bc90f026fc4cf4981b970b5b
MuJIbtuGamer <kulakov.step@gmail.com>
2024-10-30 22:33:14 +0300

    Update index.js
    DON'T WORK BotGhost

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = [/*"Слушает ваши команды 😁", */"Я больше не работаю"];
-const statusTypes = [/*'online', */'dnd'];
+const statusMessages = [/*"Слушает ваши команды 😁", */"Я на тех. работах 🛠"];
+const statusTypes = [/*'online', */'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
